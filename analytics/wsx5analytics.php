33cf389a922ece4ce7400c81843b69ba058f1c4e
vannalux <remtexvanna@gmail.com>
2022-12-28 01:51:29 +0200

    66

--- analytics/wsx5analytics.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('../res/x5engine.php');
-$db = getDbData();
-if ($db === false) { die('{ "result": "error" }'); }
-$analytics = new Analytics(ImDb::from_db_data($db), 'w5_c1be943f_analytics');
-$analytics->visit($_POST['uid'], $_POST['url'], $_POST['lang']);
-echo '{ "result": "ok" }';
